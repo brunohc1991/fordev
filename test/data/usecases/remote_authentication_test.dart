23308f33187f8682fda3e13dfc4c5ac4b815f7b9
Bruno H Costa <brunohc1991@gmail.com>
2022-05-03 08:17:13 -0300

    ensure remoteAuthentication thrwos UnexpectedError 400

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,4 +1,5 @@
 import 'package:faker/faker.dart';
+import 'package:fordev/domain/helpers/helpers.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
@@ -28,5 +29,19 @@ void main() {
         url: url,
         method: 'post',
         body: RemoteAuthenticationParams.fromDomain(params).toJson()));
+  });
+
+  test("Should throw unexpectdetError if HttpClient returns 400", () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.badRequest);
+
+    final params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
+
+    final future = sut.auth(params);
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
