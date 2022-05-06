eb196452b51b39a38b9658ec4311acdd3b5c10eb
Bruno H Costa <brunohc1991@gmail.com>
2022-05-06 08:02:26 -0300

    ensure HttpAdapter throws ServerError on 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -103,7 +103,7 @@ void main() {
     });
 
     test('Should return bad resquest erros if post return 400', () async {
-      mockResponse(400);
+      mockResponse(400, body: '');
 
       final future = sut.request(url: url, method: 'post');
 
@@ -111,11 +111,19 @@ void main() {
     });
 
     test('Should return bad resquest erros if post return 400', () async {
-      mockResponse(400, body: '');
+      mockResponse(400);
 
       final future = sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return bad resquest erros if post return 500', () async {
+      mockResponse(500);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
     });
   });
 }
