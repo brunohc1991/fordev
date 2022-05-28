b545f299b84156ec0c14e248a8445b0a76e4f7d0
Bruno H Costa <brunohc1991@gmail.com>
2022-05-28 09:00:46 -0300

    ensure StreamLoginPresenter emits form valid event if form is valid

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -3,10 +3,16 @@ import 'package:meta/meta.dart';
 import '../protocols/validation.dart';
 
 class LoginState {
+  String email;
+  String password;
   String emailError;
   String passwordError;
 
-  bool get isFormValid => false;
+  bool get isFormValid =>
+      emailError == null &&
+      passwordError == null &&
+      email != null &&
+      password != null;
 }
 
 class StreamLoginPresenter {
@@ -31,11 +37,13 @@ class StreamLoginPresenter {
   }
 
   void validateEmail(String email) {
+    _state.email = email;
     _state.emailError = validation.validate(field: 'email', value: email);
     _update();
   }
 
   void validatePassword(String password) {
+    _state.password = password;
     _state.passwordError =
         validation.validate(field: 'password', value: password);
     _update();
