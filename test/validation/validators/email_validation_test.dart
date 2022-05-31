3f7d732b5513b878104341ee7d8c81b6aaad23ef
Bruno H Costa <brunohc1991@gmail.com>
2022-05-31 08:20:06 -0300

    ensure EmailValidation returns error or null according to the scenario

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,39 @@
+import 'package:fordev/validation/protocols/field_validation.dart';
+import 'package:test/test.dart';
+
+class EmailValidation implements FieldValidation {
+  String field;
+
+  EmailValidation(this.field);
+
+  String validate(String value) {
+    final regex = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+    return isValid ? null : 'Campo inválido!';
+  }
+}
+
+void main() {
+  EmailValidation sut;
+
+  setUp(() {
+    sut = EmailValidation('any-field');
+  });
+
+  test('Should return null if email is empty', () {
+    expect(sut.validate(''), null);
+  });
+
+  test('Should return null if email is null', () {
+    expect(sut.validate(null), null);
+  });
+
+  test('Should return null if email is valid', () {
+    expect(sut.validate('brunohc1991@gmail.com'), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('brunohc1991@'), 'Campo inválido!');
+  });
+}
