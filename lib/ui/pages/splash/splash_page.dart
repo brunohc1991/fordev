9235cbe5ca6d825090f2344bf648d2b8e33d0ed9
Bruno H Costa <brunohc1991@gmail.com>
2022-06-18 10:10:33 -0300

    move splashPage to its own file

---
+++ lib/ui/pages/splash/splash_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:fordev/ui/pages/splash/splash.dart';
+import 'package:get/get.dart';
+
+class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter;
+
+  SplashPage({@required this.presenter});
+
+  @override
+  Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+    return Scaffold(
+      appBar: AppBar(title: Text('4Dev')),
+      body: Builder(
+        builder: (context) {
+          presenter.navigateToStream.listen((page) {
+            if (page?.isNotEmpty == true) {
+              Get.offAllNamed(page);
+            }
+          });
+
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
