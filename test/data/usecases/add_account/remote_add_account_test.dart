097de384101304099ffb68ed65325f054a0be2ab
Bruno H Costa <brunohc1991@gmail.com>
2022-07-02 08:45:04 -0300

    Ensure RemoteAddAccount throws UnexpectedError on 404

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -54,4 +54,11 @@ void main() {
     final future = sut.add(params);
     expect(future, throwsA(DomainError.unexpected));
   });
+
+  test("Should throw unexpectdetError if HttpClient returns 404", () async {
+    mockHttpError(HttpError.notFound);
+
+    final future = sut.add(params);
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
